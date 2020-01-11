8e0e620ebae5d8dd291ab7084f0a4f1dec944b72
Niranjan Sharma <nsniranjansharma7@gmail.com>
2020-01-11 16:27:46 +0530

    Tip Post part is done

--- src/contracts/SocialNetwork.sol
+++ src/contracts/SocialNetwork.sol
@@ -9,14 +9,21 @@ contract SocialNetwork {
         uint id;
         string content;
         uint tipAmount;
-        address author;
+        address payable author;
     }
 
     event PostCreated(
         uint id,
         string content,
         uint tipAmount,
-        address author
+        address payable author
+    );
+
+    event PostTipped(
+        uint id,
+        string content,
+        uint tipAmount,
+        address payable author
     );
 
     constructor() public {
@@ -28,5 +35,21 @@ contract SocialNetwork {
         postCount ++;
         posts[postCount] = Post(postCount, _content,0, msg.sender);
         emit PostCreated(postCount, _content,0, msg.sender);
+    }
+
+    function tipPost(uint _id) public payable {
+        // Make sure that id is valid
+        require(_id > 0 && _id <= postCount);
+        //Fetch the Post
+        Post memory _post = posts[_id];
+        //Fetch the author
+        address payable _author = _post.author;
+        //Pay the author by sending ether
+        address(_author).transfer(msg.value);
+        //Increment the tip amount
+        _post.tipAmount = _post.tipAmount + msg.value;
+        //Update the post
+        posts[_id] = _post;
+        emit PostTipped(postCount, _post.content, _post.tipAmount, _author);
     }
 }
